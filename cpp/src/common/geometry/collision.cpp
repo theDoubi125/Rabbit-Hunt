855aa81427fd807a708c38a0b85e2c6db6822cf5
doubi125 <julien.revel@telecomnancy.net>
2018-07-27 18:09:19 +0200

    wip Collision tests implementation

--- cpp/src/common/geometry/collision.cpp
+++ cpp/src/common/geometry/collision.cpp
@@ -1,16 +1,17 @@
 #include "collision.h"
 #include <climits>
+#include <cmath>
 
 namespace collision
 {
 	void testAABBCollisions(const AABB* firsts, const AABB* seconds, BitArray* outFlags, int count)
 	{
+		static unsigned char bitArraySize = CHAR_BIT * sizeof(BitArray);
 		for(int i=0; i<count; i++)
 		{
 			const AABB& A = firsts[i];
 			const AABB& B = seconds[i];
 
-			unsigned char bitArraySize = CHAR_BIT * sizeof(BitArray);
 			unsigned char flagNum = i%bitArraySize;
 			unsigned char outFlagIndex = i / bitArraySize;
 			if ((A.min.x > B.min.x && B.max.x > A.min.x) || (A.min.x <= B.min.x && A.max.x > B.min.x)
@@ -25,9 +26,24 @@ namespace collision
 		}
 	}
 
-	void testCircleCollisions(const Circle* firsts, const Circle* seconds, int* outFlags, int count)
+	void testCircleCollisions(const Circle* firsts, const Circle* seconds, BitArray* outFlags, int count)
 	{
+		const unsigned char BIT_ARRAY_SIZE = CHAR_BIT * sizeof(BitArray);
+		int cellsCount = (count + BIT_ARRAY_SIZE - 1) / BIT_ARRAY_SIZE;
+		for (int i = 0; i < cellsCount; i++)
+		{
+			outFlags[i] = 0;
+			for (int j = 0; j + i * BIT_ARRAY_SIZE < count; j++)
+			{
+				int index = j + i * BIT_ARRAY_SIZE;
+				const Circle& A = firsts[index];
+				const Circle& B = seconds[index];
+				const vec2 AB = B.pos - A.pos;
+				float distanceSquared = AB.x * AB.x + AB.y * AB.y;
 
+				outFlags[i] |= (distanceSquared < (A.r + B.r) * (A.r + B.r)) << j;
+			}
+		}
 	}
 
 	void testAABBCircleCollisions(const Circle* circle, const AABB* aabb, int* outFlags, int count)
